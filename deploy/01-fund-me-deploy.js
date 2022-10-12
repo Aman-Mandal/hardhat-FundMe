a47fa9ef750bc0628133169c9d3ef4b83ff17235
Aman-Mandal <002amankumar@gmail.com>
2022-10-12 22:31:05 +0530

    Deployed locally and on testnet and verified too

--- deploy/01-fund-me-deploy.js
+++ deploy/01-fund-me-deploy.js
@@ -1,5 +1,6 @@
 const { network } = require('hardhat')
 const { networkConfig, developmentChains } = require('../helper-hardhat-config')
+const { verify } = require('../utils/verify')
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments
@@ -19,12 +20,22 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   // for our local testing
 
   // when going for localhost or hardhat network we want to use a mock
+  const args = [ethUsdPriceFeedAddress]
 
   const fundMe = await deploy('FundMe', {
     from: deployer,
-    args: [ethUsdPriceFeedAddress], // put price feed address
-    log: true
+    args, // put price feed address
+    log: true,
+    waitConfirmations: network.config.blockConfirmations || 1,
   })
+
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    // verify contract
+    await verify(fundMe.address, args)
+  }
 
   log('-------------------------------------------------------------')
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -7,35 +7,37 @@ require('dotenv').config()
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
 const COINMARKET_API_KEY = process.env.COINMARKET_API_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   // solidity: '0.8.17',
   solidity: {
-    compilers: [{ version: '0.8.8' }, { version: '0.6.6' }]
+    compilers: [{ version: '0.8.8' }, { version: '0.6.6' }],
   },
   defaultNetwork: 'hardhat',
   networks: {
     goerli: {
       url: GOERLI_RPC_URL,
-      accounts: [],
-      chainId: 5
-    }
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+      blockConfirmations: 6,
+    },
   },
   gasReporter: {
-    enabled: true,
+    enabled: false,
     currency: 'USD',
-    coinmarketcap: COINMARKET_API_KEY
+    coinmarketcap: COINMARKET_API_KEY,
   },
   etherscan: {
-    apiKey: ETHERSCAN_API_KEY
+    apiKey: ETHERSCAN_API_KEY,
   },
   namedAccounts: {
     deployer: {
-      default: 0
+      default: 0,
     },
     user: {
-      default: 1
-    }
-  }
+      default: 1,
+    },
+  },
 }
